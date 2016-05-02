3fc5889b1c0801c7f2f062a09d3c29ced1fc2a3f
icarrera <irismcarrera@gmail.com>
2016-05-02 12:47:13 -0700

    added route for weather api data

--- pxl_master/pxl/urls.py
+++ pxl_master/pxl/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from pxl import views
+from . import views
 
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'api/v1/weather/', views.weather_api_endpoint)
 ]
