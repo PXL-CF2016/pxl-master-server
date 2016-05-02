f6ad56e7406b2affdd7e34b6633f48b044eaa5f3
Joseph McClenahan <jmcclena94@gmail.com>
2016-05-02 15:11:30 -0700

    added user model and pxlboard model

--- pxl_master/pxl/models.py
+++ pxl_master/pxl/models.py
@@ -1,19 +1,29 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
-
-LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
+from django.conf import settings
 
 
-class Snippet(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, blank=True, default='')
-    code = models.TextField()
-    linenos = models.BooleanField(default=False)
-    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
-    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+class PXLBoard(models.Model):
+    """PXL Board model."""
 
-    class Meta:
-        ordering = ('created',)
+    mlb = models.BooleanField()
+    nfl = models.BooleanField()
+    nhl = models.BooleanField()
+    headlines = models.BooleanField()
+    weather = models.BooleanField()
+
+
+class UserModel(models.Model):
+    """PXL User model."""
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+    pxlboard_1 = models.OneToOneField(PXLBoard)
+    pxlboard_2 = models.OneToOneField(PXLBoard)
+    pxlboard_3 = models.OneToOneField(PXLBoard)
+
+    @property
+    def is_active(self):
+        return self.user.is_active
