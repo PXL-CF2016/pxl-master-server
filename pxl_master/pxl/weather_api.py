95479866438d838b37f0e757346a12cdf3a31cc1
icarrera <irismcarrera@gmail.com>
2016-05-02 11:04:27 -0700

    separated api request from filtering and return wanted data in json format.

--- pxl_master/pxl/weather_api.py
+++ pxl_master/pxl/weather_api.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 import os
 from urllib.request import urlopen
@@ -6,15 +7,21 @@ import json
 WEATHER_URL = os.environ.get('WEATHER_API_URL')
 
 
-def weather_api():
+def weather_api_call():
     """API call for current Seattle weather data."""
     weather_data = urlopen(WEATHER_URL)
     json_string = (weather_data.read()).decode('utf-8')
     parsed_json = json.loads(json_string)
+    return parsed_json
+
+def weather_api_json():
+    """Grab temperature, forecast, and location data and return as json."""
+    parsed_json = weather_api_call()
     temp = parsed_json['current_observation']['temperature_string']
     location = parsed_json['current_observation']['display_location']['full']
     weather = parsed_json['current_observation']['icon']
-    return print("{} and {} in {}".format(temp, weather, location))
-
-if __name__ == "__main__":
-    weather_api()
+    json_format = {
+        'location': location,
+        'temperature': temp,
+        'weather': weather}
+    return print(json_format)
