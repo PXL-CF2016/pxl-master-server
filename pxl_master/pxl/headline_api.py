adf2a73d138495dcb2ea8c0eb3f0c78459677774
Joseph McClenahan <jmcclena94@gmail.com>
2016-05-02 11:04:18 -0700

    Moved NPR secret key to environment.  Added docstrings.

--- pxl_master/pxl/headline_api.py
+++ pxl_master/pxl/headline_api.py
@@ -1,10 +1,28 @@
 import requests
-import json
+import os
 
 
-headline_endpoint = 'http://api.npr.org/query?id=1004&fields=title&dateType=story&output=JSON&apiKey=MDI0MTM4NTA1MDE0NjIyMDU0OTM1ZDUwNQ000'
+HEADLINE_ENDPOINT = ''.join([
+    'http://api.npr.org/query?id=1004&fields=',
+    'title&dateType=story&output=JSON&apiKey=',
+    os.environ.get('NPR_KEY')
+])
 
 
 def get_headline_data():
-    headline_data = requests.get(headline_endpoint)
+    """Generate a response from the NPR headline endpoint."""
+    headline_data = requests.get(HEADLINE_ENDPOINT)
     return headline_data.json()['list']['story']
+
+
+def form_headline_json():
+    """Form a JSON response for the NPR headline data."""
+    headlines = get_headline_data()
+    headline_json = {'headlines': []}
+    for headline in headlines:
+        title = headline['title']['$text']
+        headline_dict = {
+            'title': title
+        }
+        headline_json['headlines'].append(headline_dict)
+    return json.dumps(headline_json)
