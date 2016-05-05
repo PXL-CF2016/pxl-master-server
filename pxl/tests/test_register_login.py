b7d7d605e6cc6e4c8ac00bef80a358bc6f6ced2c
icarrera <irismcarrera@gmail.com>
2016-05-05 10:38:05 -0700

    Created login and registration tests for presence of tokens in the response.

--- pxl/tests/test_register_login.py
+++ pxl/tests/test_register_login.py
@@ -5,6 +5,7 @@ from rest_framework.test import APIClient
 from rest_framework.test import APITestCase
 from rest_framework.test import APIRequestFactory
 from mock import patch
+import json
 
 class TestRegistration(APITestCase):
     """Test Class Registration API View."""
@@ -24,17 +25,15 @@ class TestRegistration(APITestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(User.objects.count(), 1)
 
-    # @patch('pxl.views.return_credentials')
-    # def test_create_account_400(self, return_credentials):
-    #     """Test that registration form is incomplete and user is not saved."""
-    #     client = APIClient()
-    #     mocked_function = return_credentials
-    #     mocked_function.return_value = {'username': 'bldkfjsdlkfj', 'password': ''}
-    #     response = client.post('/api/v1.0/registration/')
-    #     self.assertEqual(response.status_code, 400)
-    #     self.assertEqual(User.objects.count(), 0)
-    #     # this doesn't work rignt now
-
+    @patch('pxl.views.return_credentials')
+    def test_returned_token(self, return_credentials):
+        """Test that user can register and user is added to the database."""
+        client = APIClient()
+        mocked_function = return_credentials
+        mocked_function.return_value = {'username': 'tester', 'password': 'iluvtests123'}
+        response = client.post('/api/v1.0/registration/')
+        token_string = response.content.decode("utf-8")
+        self.assertIn("token", token_string)
 
 class TestLogin(APITestCase):
     """Test Class for Login API View."""
@@ -51,12 +50,30 @@ class TestLogin(APITestCase):
         self.assertEqual(logged_in.status_code, 200)
 
     @patch('pxl.views.return_credentials')
-    def test_invalid_password(self, return_credentials):
+    def test_returned_token(self, return_credentials):
+        """Expect token returned if login is successful."""
+        mocked_function = return_credentials
+        mocked_function.return_value = {'username': 'bob', 'password': 'secretsecret'}
+        logged_in = self.client.post('/api/v1.0/login/')
+        token_string = logged_in.content.decode("utf-8")
+        self.assertIn("token", token_string)
+
+    @patch('pxl.views.return_credentials')
+    def test_bad_password(self, return_credentials):
         """Expect login failure if invalid password."""
         mocked_function = return_credentials
         mocked_function.return_value = {'username': 'bob', 'password': 'l33t49ck3r'}
         logged_in = self.client.post('/api/v1.0/login/')
         self.assertEqual(logged_in.status_code, 401)
+
+    @patch('pxl.views.return_credentials')
+    def test_bad_password_token(self, return_credentials):
+        """Expect no token returned if password is incorrect."""
+        mocked_function = return_credentials
+        mocked_function.return_value = {'username': 'bob', 'password': 'l33t49ck3r'}
+        logged_in = self.client.post('/api/v1.0/login/')
+        token_string = logged_in.content.decode("utf-8")
+        self.assertIn("Invalid username/password.", token_string)
 
     @patch('pxl.views.return_credentials')
     def test_no_password(self, return_credentials):
@@ -67,9 +84,27 @@ class TestLogin(APITestCase):
         self.assertEqual(logged_in.status_code, 401)
 
     @patch('pxl.views.return_credentials')
+    def test_no_password_token(self, return_credentials):
+        """Expect no token returned if password field is empty."""
+        mocked_function = return_credentials
+        mocked_function.return_value = {'username': 'bob', 'password': ''}
+        logged_in = self.client.post('/api/v1.0/login/')
+        token_string = logged_in.content.decode("utf-8")
+        self.assertIn("Invalid username/password.", token_string)
+
+    @patch('pxl.views.return_credentials')
     def test_invalid_user(self, return_credentials):
         """Expect login failure for invalid user."""
         mocked_function = return_credentials
         mocked_function.return_value = {'username': '48ck3r', 'password': 'l33t12345'}
         logged_in = self.client.post('/api/v1.0/login/')
         self.assertEqual(logged_in.status_code, 401)
+
+    @patch('pxl.views.return_credentials')
+    def test_invalid_user_token(self, return_credentials):
+        """Expect no token returned for invalid user login attempt."""
+        mocked_function = return_credentials
+        mocked_function.return_value = {'username': '48ck3r', 'password': 'l33t12345'}
+        logged_in = self.client.post('/api/v1.0/login/')
+        token_string = logged_in.content.decode("utf-8")
+        self.assertIn("Invalid username/password.", token_string)
