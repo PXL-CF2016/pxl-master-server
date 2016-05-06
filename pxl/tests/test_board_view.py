ab37c62b2d4414b447c2c1811cf734c3efa9a351
Joseph McClenahan <jmcclena94@gmail.com>
2016-05-05 17:59:59 -0700

    fixed view tests after update

--- pxl/tests/test_board_view.py
+++ pxl/tests/test_board_view.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 import factory
 from rest_framework.test import APIRequestFactory
+from mock import patch
+
 
 class UserFactory(factory.django.DjangoModelFactory):
     """Set up a user."""
@@ -21,44 +23,57 @@ class BoardViewTests(TestCase):
         self.client = Client()
         self.user = UserFactory.create()
 
-    def test_board_view_post_request(self):
+    @patch('pxl.views.generate_display')
+    def test_board_view_post_request(self, generate_display):
         """Test board view post request."""
+        mock_method = generate_display
+        mock_method.return_value = ''
         token = Token.objects.get(user=self.user)
         params = {
             'token': token.key,
-            'mlb': 'true',
-            'nhl': 'false',
-            'nfl': 'false',
-            'headlines': 'true',
-            'weather': 'true',
+            'mlb': True,
+            'nhl': True,
+            'nfl': True,
+            'headlines': True,
+            'weather': True,
         }
         response = self.client.post('/api/v1.0/board_data/', params)
         board = PXLBoardModel.objects.get(owner=self.user)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['token'], token.key)
         self.assertEqual(board.mlb, True)
-        self.assertEqual(board.nhl, False)
-        self.assertEqual(board.nfl, False)
+        self.assertEqual(board.nhl, True)
+        self.assertEqual(board.nfl, True)
         self.assertEqual(board.headlines, True)
         self.assertEqual(board.weather, True)
 
-    def test_board_view_post_request_already_created(self):
+    @patch('pxl.views.generate_display')
+    def test_board_view_post_request_already_created(self, generate_display):
         """Test board view with an already created pxlboard."""
+        mock_method = generate_display
+        mock_method.return_value = ''
         token = Token.objects.get(user=self.user)
         params = {
-            'token': token.key,
-            'mlb': 'true',
-            'nhl': 'false',
-            'nfl': 'false',
-            'headlines': 'true',
-            'weather': 'true',
+            'mlb': '',
+            'nhl': '',
+            'nfl': '',
+            'headlines': '',
+            'weather': '',
         }
-        response = self.client.post('/api/v1.0/board_data/', params)
+        new_board = PXLBoardModel(owner=self.user, **params)
+        new_board.save()
+        board_instance = PXLBoardModel.objects.get(owner=self.user)
+        self.assertEqual(board_instance.mlb, False)
+        self.assertEqual(board_instance.nfl, False)
+        self.assertEqual(board_instance.nhl, False)
+        self.assertEqual(board_instance.weather, False)
+        self.assertEqual(board_instance.headlines, False)
+        self.assertEqual(board_instance.owner, self.user)
         newparams = {
             'token': token.key,
-            'mlb': 'false',
+            'mlb': 'true',
             'nhl': 'true',
-            'nfl': 'false',
+            'nfl': 'true',
             'headlines': 'true',
             'weather': 'true',
         }
@@ -66,8 +81,8 @@ class BoardViewTests(TestCase):
         board = PXLBoardModel.objects.get(owner=self.user)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['token'], token.key)
-        self.assertEqual(board.mlb, False)
+        self.assertEqual(board.mlb, True)
         self.assertEqual(board.nhl, True)
-        self.assertEqual(board.nfl, False)
+        self.assertEqual(board.nfl, True)
         self.assertEqual(board.headlines, True)
         self.assertEqual(board.weather, True)
